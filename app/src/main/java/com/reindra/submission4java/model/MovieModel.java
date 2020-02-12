44cd930a30e83e03c2aaed8f2aba56524cf9ad17
Reindra12 <reindrairawan@gmail.com>
2020-02-12 12:30:49 +0700

    set data api tv

--- app/src/main/java/com/reindra/submission4java/model/MovieModel.java
+++ app/src/main/java/com/reindra/submission4java/model/MovieModel.java
@@ -66,3 +66,48 @@ public class MovieModel extends ViewModel {
     }
 
 }
+    public void setTV() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        final ArrayList<Movie> listItems = new ArrayList<>();
+        String url = "https://api.themoviedb.org/3/discover/tv?api_key=" + API_KEY + "&language=en-US";
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
+                try {
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("results");
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject movie = list.getJSONObject(i);
+                        Movie items = new Movie();
+                        items.setId(movie.getInt("id"));
+                        items.setTitle(movie.getString("name"));
+                        items.setOverview(movie.getString("overview"));
+                        items.setDate(movie.getString("first_air_date"));
+                        items.setRating(movie.getString("vote_average"));
+                        items.setCountry(movie.getString("origin_country"));
+                        items.setPhoto("https://image.tmdb.org/t/p/w185" + movie.getString("poster_path"));
+                        listItems.add(items);
+                    }
+                    ListMovies.postValue(listItems);
+                } catch (Exception e) {
+                    Log.d("Exception", e.getMessage());
+
+                }
+            }
+
+            @Override
+
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("onFailure", error.getMessage());
+
+            }
+        });
+    }
+
+    public LiveData<ArrayList<Movie>> getTV() {
+        return ListMovies;
+
+    }
+
+}
