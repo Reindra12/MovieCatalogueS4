80b0f6313090b3a88960e417ef18d6508c2d4b25
Reindra12 <reindrairawan@gmail.com>
2020-03-01 15:22:47 +0700

    ganti toolbar

--- app/src/main/java/com/reindra/submission4java/activity/MainActivity.java
+++ app/src/main/java/com/reindra/submission4java/activity/MainActivity.java
@@ -10,34 +10,50 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.reindra.submission4java.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView title = findViewById(R.id.tv_titlebar);
+      /*  TextView title = findViewById(R.id.tv_titlebar);
         title.setText(R.string.title_movie);
         ImageView language = findViewById(R.id.imgsetting);
-        language.setOnClickListener(this);
+        ImageView search = findViewById(R.id.imgsearch);
+        ImageView notif = findViewById(R.id.imgnotif);*/
+
+      /*  search.setOnClickListener(this);
+        notif.setOnClickListener(this);
+        language.setOnClickListener(this);*/
+
+        AppBarConfiguration appbar = new AppBarConfiguration.Builder(R.id.nav_movie, R.id.nav_tv, R.id.nav_fav)
+                .build();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
+       NavigationUI.setupActionBarWithNavController(this, navController,appbar);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
-    @Override
+   /* @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.imgsetting:
                 Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
                 startActivity(intent);
                 break;
+            case R.id.imgnotif:
+                Intent search = new Intent(this, NotificationActivity.class);
+                startActivity(search);
+
+
         }
-    }
+    }*/
 }
