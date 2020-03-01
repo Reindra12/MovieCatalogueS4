338afd01fb3489d8e39cc235e44a1b73958c0774
Reindra12 <reindrairawan@gmail.com>
2020-03-01 15:32:59 +0700

    falidasi notif

--- app/src/main/java/com/reindra/submission4java/activity/NotificationActivity.java
+++ app/src/main/java/com/reindra/submission4java/activity/NotificationActivity.java
@@ -1,3 +1,15 @@
+        swDaily.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean dailyIsChecked) {
+                checkDaily = dailyIsChecked;
+                setPref();
+                if (dailyIsChecked) {
+                    dailyReceiver.dailyNotificationOn(NotificationActivity.this);
+                } else {
+                    dailyReceiver.dailyNotifOff(NotificationActivity.this);
+                }
+            }
+        });
     private void setPref() {
         SharedPreferences sharedPreferences = getSharedPreferences(SETTING_PREFS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
